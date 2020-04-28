3efcd11c0697aa277bffad6108fa83ba96b175f7
harisahmedDCS <ahmedharis837@gmail.com>
2020-04-29 01:22:49 +0500

    Added readme and starter config

---
+++ controllers/reviews.js
@@ -0,0 +1,117 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middle/async');
+const Review = require('../models/Review');
+const Bootcamp = require('../models/Bootcamp');
+
+//@desc   Get Reviews
+//@route Get /api/v1/reviews
+//@route Get /api/v1/bootcamps/:bootcampId/reviews
+//@access Pubic
+exports.getReviews = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
+
+//@desc   Get single Reviews
+//@route Get /api/v1/reviews/:id
+//@access Pubic
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+//@desc  Add Reviews
+//@route POST /api/v1/bootcamps/:bootcampId/reviews
+//@access Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp with the Id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const review = await Review.create(req.body);
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+//@desc  Update Reviews
+//@route Put /api/v1/reviews/:id
+//@access Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No bootcamp with the Id of ${req.params.id}`, 404)
+    );
+  }
+
+  //Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update a review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+//@desc  Delete Reviews
+//@route Delete /api/v1/reviews/:id
+//@access Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No bootcamp with the Id of ${req.params.id}`, 404)
+    );
+  }
+
+  //Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update a review`, 401));
+  }
+
+  await review.remove();
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- server.js
+++ server.js
@@ -1,14 +1,93 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const errorHandler = require('./middle/error');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamp');
+const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+
 const app = express();
 
+//Body parser
+
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//File uploading
+app.use(fileupload());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set Security header
+app.use(helmet());
+
+//Prevent xss attacks
+app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+
+//prevent http param pollution
+app.use(hpp());
+
+//Enabled CORS
+app.use(cors());
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Mount routers¨
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`.red.bold);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
